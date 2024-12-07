9c80e05a3c10653ccdbf792405cb4723853aa7c7
Nikola Sevo <nsevonikola@gmail.com>
2024-12-07 20:00:52 +0100

    Added ts plugin

--- init.lua
+++ init.lua
@@ -33,6 +33,6 @@ require("lazy").setup({
 	require("plugins.gitsigns"),
 	require("plugins.alpha"),
 	require("plugins.indent-blankline"),
-	--    require 'plugins.typescript-tools',
+	require("plugins.typescript-tools"),
 	require("plugins.misc"),
 })
