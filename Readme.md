04f00337679703288e4ec579e3809be56e79e9e1
Nikola Sevo <nsevonikola@gmail.com>
2024-12-07 19:45:56 +0100

    Base nvim confing in place

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Nvim configuration
+
+
+## Requirements
+
+- nvim
+
+## Fonts & Icons
+Any nerd font, i like *JetBransMono*
+
+## nvim treesitter
+ - A C compiler
+
+### For telescope
+- fd
+- ripgrep
+
+
---
+++ init.lua
@@ -0,0 +1,38 @@
+require("core.keymaps")
+require("core.options")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
+
+vim.opt.rtp:prepend(lazypath)
+
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require("lazy").setup({
+	require("plugins.neo-tree"),
+	require("plugins.colorscheme"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.none-ls"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	require("plugins.indent-blankline"),
+	--    require 'plugins.typescript-tools',
+	require("plugins.misc"),
+})
