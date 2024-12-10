5b3275988925ad88579cc30a3cedd65302fbf53e
Nikola Sevo <nikola.sevo@ictech.se>
2024-12-10 11:12:48 +0100

    Lua issue in ts-tools, fixed

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -4,11 +4,11 @@ return {
 	opts = {},
 	config = function()
 		require("typescript-tools").setup({
+			on_attach = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end,
 			settings = {
-				on_attach = function(client)
-					client.server_capabilities.documentFormattingProvider = false
-					client.server_capabilities.documentRangeFormattingProvider = false
-				end
 				-- spawn additional tsserver instance to calculate diagnostics on it
 				separate_diagnostic_server = true,
 				-- "change"|"insert_leave" determine when the client asks the server about diagnostic
