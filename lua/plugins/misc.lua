c516a41c954536c659494adf0bee54f2022ae9f7
Nikola Sevo <nsevonikola@gmail.com>
2024-12-11 21:35:05 +0100

    Init auto close tags plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,23 +2,26 @@
 return {
   {
     -- autoclose tags
-    'windwp/nvim-ts-autotag',
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
   },
   {
     -- detect tabstop and shiftwidth automatically
-    'tpope/vim-sleuth',
+    "tpope/vim-sleuth",
   },
   {
     -- Powerful Git integration for Vim
-    'tpope/vim-fugitive',
+    "tpope/vim-fugitive",
   },
   {
     -- GitHub integration for vim-fugitive
-    'tpope/vim-rhubarb',
+    "tpope/vim-rhubarb",
   },
   {
     -- Hints keybinds
-    'folke/which-key.nvim',
+    "folke/which-key.nvim",
     opts = {
       -- win = {
       --   border = {
@@ -36,23 +39,23 @@ return {
   },
   {
     -- Autoclose parentheses, brackets, quotes, etc.
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
     config = true,
     opts = {},
   },
   {
     -- Highlight todo, notes, etc in comments
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = false },
   },
   {
     -- high-performance color highlighter
-    'norcalli/nvim-colorizer.lua',
+    "norcalli/nvim-colorizer.lua",
     config = function()
-      require('colorizer').setup()
+      require("colorizer").setup()
     end,
   },
 }
