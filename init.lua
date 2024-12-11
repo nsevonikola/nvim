3bc5b3b14072427a50ffcfc6827f2ac3738fdd43
Nikola Sevo <nsevonikola@gmail.com>
2024-12-11 20:49:09 +0100

    Disable lualine

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
 	require("plugins.neo-tree"),
 	require("plugins.colorscheme"),
 	require("plugins.bufferline"),
-	require("plugins.lualine"),
+	-- require("plugins.lualine"),
 	require("plugins.treesitter"),
 	require("plugins.telescope"),
 	require("plugins.lsp"),
