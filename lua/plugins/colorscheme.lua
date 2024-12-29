3bb8e7f313bca4b6a2e5a4c7f7b738602a8f5b68
Nikola Sevo <nsevonikola@gmail.com>
2024-12-30 00:19:20 +0100

    Hidden  but optional transparent bg gutter

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -31,6 +31,9 @@ return {
 				return {}
 			end,--]]
 			theme = "wave", -- Load "wave" theme when 'background' option is not set
+			-- colors = {
+			-- 	theme = { all = { ui = { bg_gutter = "none" } } },
+			-- },
 			background = { -- map the value of 'background' option to a theme
 				dark = "wave", -- try "dragon" !
 				light = "lotus",
