5503316d65f3ee233b281eecab8f354e05af272e
Nikola Sevo <nsevonikola@gmail.com>
2024-12-30 11:37:44 +0100

    no wrap

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,3 @@
---[[return {
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-	require('nordic').load()
-    end
-}--]]
-
 return {
 	"rebelot/kanagawa.nvim",
 	lazy = false,
@@ -23,17 +14,17 @@ return {
 			transparent = false, -- do not set background color
 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			--[[colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-			},
+			-- colors = { -- add/modify theme and palette colors
+			-- 	palette = {},
+			-- 	theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+			-- },
 			overrides = function(colors) -- add/modify highlights
 				return {}
-			end,--]]
+			end,
 			theme = "wave", -- Load "wave" theme when 'background' option is not set
-			-- colors = {
-			-- 	theme = { all = { ui = { bg_gutter = "none" } } },
-			-- },
+			colors = {
+				theme = { all = { ui = { bg_gutter = "none" } } },
+			},
 			background = { -- map the value of 'background' option to a theme
 				dark = "wave", -- try "dragon" !
 				light = "lotus",
