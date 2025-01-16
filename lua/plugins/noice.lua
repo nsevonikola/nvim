b1436217709453bd1a1c85ae4952f35e6b597030
Nikola Sevo <nikola.sevo@ictech.se>
2025-01-16 16:35:28 +0100

    Noice search centered. Tried to remove enoying noice 'saved' message. to be continued..

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -25,18 +25,32 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = false,    -- use a classic bottom cmdline for search
-				command_palette = true,   -- position the cmdline and popupmenu together
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false,   -- add a border to hover docs and signature help
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 			routes = {
+				{ -- doesnt work
+					filter = {
+						event = "msg_show",
+						kind = "written",
+					},
+					opts = { skip = true },
+				},
+				{
+					filter = {
+						event = "msg_show",
+						kind = "saved",
+					},
+					opts = { skip = true },
+				},
 				{
 					filter = {
 						event = "msg_show",
 						kind = "",
-						find = "written",
+						find = "nohlsearch",
 					},
 					opts = { skip = true },
 				},
