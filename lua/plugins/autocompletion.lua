fe73acaca5173d28224fb9ffc732c79730cad2ea
Nikola Sevo <nikola.sevo@ictech.se>
2025-04-07 15:42:41 +0200

    Autocompeltion update. Some keybindings and no default show on description

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -25,7 +25,12 @@ return {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "enter" },
+		keymap = {
+			preset = "enter",
+			["Esc"] = { "hide" },
+			["<C-Space>"] = { "show" },
+			["<C-c>"] = { "show" },
+		},
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -34,7 +39,9 @@ return {
 		},
 
 		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
+		completion = {
+			documentation = { auto_show = false },
+		},
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
